0cfb2c1ae96d81b192f79dc75696031eb5599551
MBANS8A1 <seanmbng@aol.com>
2025-05-13 09:46:00 +0100

    Added an introductory pizzeria message (<p>) inside the JSX mode of the Menu component and wrapped it in a React fragment with the <ul> list of pizzas.

--- src/index.js
+++ src/index.js
@@ -73,12 +73,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative pizza dishes to choose from.
+            All from our stone oven, all organic and all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>I am still working on my menu. Please return later :)</p>
       )}
